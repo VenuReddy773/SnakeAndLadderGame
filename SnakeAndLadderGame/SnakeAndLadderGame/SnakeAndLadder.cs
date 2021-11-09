84ca18561c27e3ca2ba7c9e1a822cf1959041d69
“VenuReddy773” <venu706@gmail.com>
2021-11-09 22:23:46 +0530

    [VenuGopalReddy]: Player Rolls a Die

--- SnakeAndLadderGame/SnakeAndLadderGame/SnakeAndLadder.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/SnakeAndLadder.cs
@@ -7,13 +7,11 @@ namespace SnakeAndLadderGame
     class SnakeAndLadder
     {
         public int PlayerPosition = 0;
-       public void DieRoll()
+        public void DieRoll()
         {
             Random random = new Random();
             int Dice = random.Next(1, 7);           
             Console.WriteLine("Current position of Player is:" + Dice);
         }
-
-    }
-       
+    }       
 }
