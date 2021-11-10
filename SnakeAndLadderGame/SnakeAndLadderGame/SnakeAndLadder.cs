5a2a87fbd9a150b8521626a74cbe14fbd6409b8b
“VenuReddy773” <venu706@gmail.com>
2021-11-10 12:50:05 +0530

    [VenuGopalReddy] Added Roll a Die Feature

--- SnakeAndLadderGame/SnakeAndLadderGame/SnakeAndLadder.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/SnakeAndLadder.cs
@@ -6,12 +6,12 @@ namespace SnakeAndLadderGame
 {
     class SnakeAndLadder
     {
-        public int PlayerPosition = 0;
+        public int playerPosition = 0;
         public void DieRoll()
         {
             Random random = new Random();
             int Dice = random.Next(1, 7);           
-            Console.WriteLine("Current position of Player is:" + Dice);
+            Console.WriteLine("Rolled Die is:" + Dice);
         }
     }       
 }
